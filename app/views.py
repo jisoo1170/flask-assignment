efc545972520684212ca62ed33a14d5921b40c32
Jisoo Choi <cjisoo0117@gmail.com>
2020-07-07 18:00:56 +0900

    글 작성 기능 추가

--- app/views.py
+++ app/views.py
@@ -1,13 +1,26 @@
 from flask_classful import FlaskView
+from flask import request
 
 from app import app
 from app.models import Board
 
 
 class BoardView(FlaskView):
+    route_base = '/'
+
     def get(self):
         board = Board.objects()
         return board.to_json()
-    
+
+    def post(self):
+        try:
+            title = request.values.get('title')
+            content = request.values.get('content')
+            board = Board(title=title, content=content)
+            board.save()
+            return {'message': '저장 완료!'}
+        except Exception:
+            return {'error': '글을 저장하지 못했습니다'}
+
 
 BoardView.register(app)

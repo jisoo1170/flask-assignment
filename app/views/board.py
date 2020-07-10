a9281c5013852cb6ed625202876ff0462b945048
Jisoo Choi <cjisoo0117@gmail.com>
2020-07-10 11:07:39 +0900

    게시글 사용자 권한 추가

--- app/views/board.py
+++ app/views/board.py
@@ -3,6 +3,7 @@ from flask import request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from app.models.board import Board
+from app.models.user import User
 from app.serailziers.board import BoardSchema
 
 
@@ -30,13 +31,24 @@ class BoardView(FlaskView):
         except Exception:
             return {'error': '글을 저장하지 못했습니다'}, 404
 
+    @jwt_required
     def put(self, pk):
+        board = Board.objects.get(pk=pk)
+        user = User.objects.get(pk=get_jwt_identity())
+        # 권한 확인
+        if board.user != user:
+            return {'error': '권한이 없습니다'}, 401
         title = request.values.get('title')
         content = request.values.get('content')
-        board = Board.objects.get(pk=pk)
-        board.update(title=title, content=content)
+        board.modify(title=title, content=content)
         return self.schema.dumps(board), 200
 
+    @jwt_required
     def delete(self, pk):
-        Board.objects(pk=pk).delete()
+        board = Board.objects.get(pk=pk)
+        user = User.objects.get(pk=get_jwt_identity())
+        # 권한 확인
+        if board.user != user:
+            return {'error': '권한이 없습니다'}, 401
+        board.delete()
         return {'message': '삭제 완료!'}, 200

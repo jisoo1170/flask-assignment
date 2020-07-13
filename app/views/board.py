f08031b7a4af3291805b492297340eb0386dcc5e
Jisoo Choi <cjisoo0117@gmail.com>
2020-07-13 14:23:38 +0900

    댓글 수정 기능 추가

--- app/views/board.py
+++ app/views/board.py
@@ -4,7 +4,7 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from app.models.board import Board, Comment
 from app.models.user import User
-from app.serailziers.board import BoardSchema
+from app.serailziers.board import BoardSchema, CommentSchema
 
 
 class BoardView(FlaskView):
@@ -61,26 +61,28 @@ class CommentView(FlaskView):
 
     @jwt_required
     def post(self, board_pk):
-        comment = request.values.get('content')
+        content = request.values.get('content')
         user = User.objects.get(pk=get_jwt_identity())
 
-        comment = Comment(user=user, comment=comment)
+        comment = Comment(user=user, content=content)
 
         board = Board.objects.get(pk=board_pk)
         board.comments.append(comment)
         board.save()
         return BoardSchema().dumps(board), 201
 
-    # @jwt_required
-    # def put(self, board_pk, pk):
-    #     content = request.values.get('content')
-    #     user = User.objects.get(pk=get_jwt_identity())
-    #
-    #     comment = Comment.objects.get(pk=pk)
-    #
-    #     if comment.user != user:
-    #         return {'error': '권한이 없습니다'}, 401
-    #
-    #     board = Board.objects.get(pk=board_pk)
-    #     comment.modify(content=content)
-    #     return BoardSchema().dumps(board), 200
+    @jwt_required
+    def put(self, board_pk, pk):
+        content = request.values.get('content')
+        user = User.objects.get(pk=get_jwt_identity())
+
+        board = Board.objects.get(pk=board_pk)
+        comment = board.comments.get(id=pk)
+
+        if comment.user != user:
+            return {'error': '권한이 없습니다'}, 401
+
+        comment.content = content
+        board.save()
+
+        return BoardSchema().dumps(board), 200

371032f227c109711a38ab2d20fc91e95ae38d45
Jisoo Choi <cjisoo0117@gmail.com>
2020-07-24 14:06:23 +0900

    develop, test 환경 분리
    환경 변수 'APP_ENV'를 통해 develop, test 환경을 변경하게 수정함. test 환경에는 mongomock을 사용

--- app/config.py
+++ app/config.py
@@ -2,6 +2,19 @@ import datetime
 
 
 class Config(object):
-    DEBUG = True
     JSON_AS_ASCII = False
     JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(days=20)
+    SECRET_KEY = '@6f%$q&arc4cqnnw!q31@%#b6yly_v1pw6j6celpryeewio&@8'
+    MONGODB_DB = 'tutorial'
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestConfig(Config):
+    MONGODB_HOST = 'mongomock://localhost'
+    TESTING = True
+
+
+config_by_name = dict(dev=DevelopmentConfig, test=TestConfig)
--- app/views/comment.py
+++ app/views/comment.py
@@ -11,27 +11,27 @@ from app.views import get_paginated_list
 
 
 class CommentView(FlaskView):
-    def index(self, post):
-        Post.objects.get_or_404(id=post)
-        comments = Comment.objects(post=post).order_by('-num_of_likes')
+    def index(self, post_id):
+        Post.objects.get_or_404(id=post_id)
+        comments = Comment.objects(post=post_id).order_by('-num_of_likes')
         return jsonify(get_paginated_list(
             model='comments', results=comments, schema=CommentSchema(),
-            url=f'/posts/{post}/comments', params='',
+            url=f'/posts/{post_id}/comments', params='',
             start=int(request.args.get('start', 1)), limit=20
         )), 200
 
     @jwt_required
-    def post(self, post):
+    def post(self, post_id):
         try:
             data = CommentSchema().load(request.json)
         except ValidationError as err:
             return err.messages, 400
-        comment = Comment(user=get_jwt_identity(), post=post, **data)
+        comment = Comment(user=get_jwt_identity(), post=post_id, **data)
         comment.save()
         return CommentSchema().dump(comment), 201
 
     @jwt_required
-    def patch(self, post, id):
+    def patch(self, post_id, id):
         comment = Comment.objects.get_or_404(id=id)
         user = User.objects.get(id=get_jwt_identity())
         if comment.user != user:
@@ -44,7 +44,7 @@ class CommentView(FlaskView):
         return CommentSchema().dump(comment), 200
 
     @jwt_required
-    def delete(self, post, id):
+    def delete(self, post_id, id):
         comment = Comment.objects.get_or_404(id=id)
         user = User.objects.get(id=get_jwt_identity())
         # 권한 확인
@@ -55,7 +55,7 @@ class CommentView(FlaskView):
 
     @jwt_required
     @route('/<id>/likes', methods=['POST'])
-    def like(self, post, id):
+    def like(self, post_id, id):
         comment = Comment.objects.get_or_404(id=id)
         user = User.objects.get(id=get_jwt_identity())
         # 이미 좋아요를 누른 경우 좋아요 취소
@@ -70,16 +70,16 @@ class CommentView(FlaskView):
 class RecommentView(FlaskView):
     decorators = [jwt_required]
 
-    def post(self, post, comment):
+    def post(self, post_id, comment_id):
         try:
             data = RecommentSchema().load(request.json)
         except ValidationError as err:
             return err.messages, 400
-        recomment = Recomment(user=get_jwt_identity(), comment=comment, **data)
+        recomment = Recomment(user=get_jwt_identity(), comment=comment_id, **data)
         recomment.save()
         return RecommentSchema().dump(recomment), 201
 
-    def patch(self, post, comment, id):
+    def patch(self, post_id, comment_id, id):
         recomment = Recomment.objects.get_or_404(id=id)
         user = User.objects.get(id=get_jwt_identity())
         if recomment.user != user:
@@ -91,7 +91,7 @@ class RecommentView(FlaskView):
         recomment.modify(**data)
         return RecommentSchema().dump(recomment), 200
 
-    def delete(self, post, comment, id):
+    def delete(self, post_id, comment_id, id):
         recomment = Recomment.objects.get_or_404(id=id)
         user = User.objects.get(id=get_jwt_identity())
         if recomment.user != user:
@@ -100,7 +100,7 @@ class RecommentView(FlaskView):
         return {}, 204
 
     @route('/<id>/likes', methods=['POST'])
-    def like(self, post, comment, id):
+    def like(self, post_id, comment_id, id):
         recomment = Recomment.objects.get_or_404(id=id)
         user = User.objects.get(id=get_jwt_identity())
         # 이미 좋아요를 누른 경우 좋아요 취소

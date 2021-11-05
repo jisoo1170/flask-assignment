eb4674ae87c5837012930fe895b6b784163626c2
jisoo <cjisoo0117@gmail.com>
2021-11-05 18:25:31 +0900

    pre-commit 테스트

--- app/serailziers/post.py
+++ app/serailziers/post.py
@@ -4,31 +4,31 @@ from .user import UserSchema
 
 not_empty = validate.Length(min=1)
 fields.Field.default_error_messages = {
-        "required": "Missing data for required field.",
-        "null": "Field may not be null.",
-        "validator_failed": "Invalid value.",
-    }
+    "required": "Missing data for required field.",
+    "null": "Field may not be null.",
+    "validator_failed": "Invalid value.",
+}
 
 
 class PostSchema(Schema):
     id = fields.String(dump_only=True)
-    user = fields.Nested(UserSchema, only=['username'], dump_only=True)
+    user = fields.Nested(UserSchema, only=["username"], dump_only=True)
     title = fields.String(required=True, validate=not_empty)
     content = fields.String(required=True, validate=not_empty)
     tags = fields.List(fields.String())
-    likes = fields.List(fields.Nested(UserSchema, only=['username']), dump_only=True)
+    likes = fields.List(fields.Nested(UserSchema, only=["username"]), dump_only=True)
     num_of_likes = fields.Integer(dump_only=True)
     num_of_views = fields.Integer(dump_only=True)
 
 
 class PostCreateSchema(PostSchema):
     class Meta:
-        fields = ('title', 'content', 'tags')
+        fields = ("title", "content", "tags")
 
 
 class PostUpdateSchema(PostSchema):
     class Meta:
-        fields = ('title', 'content', "tags")
+        fields = ("title", "content", "tags")
 
 
 class PostPaginationSchema(Schema):
@@ -36,5 +36,3 @@ class PostPaginationSchema(Schema):
     page = fields.Integer(missing=1)
     per_page = fields.Integer(missing=10)
     total = fields.Integer()
-
-

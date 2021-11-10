4e73a45caff521d0e712c828a0293e5c75e6da51
jisoo <cjisoo0117@gmail.com>
2021-11-10 18:34:06 +0900

    변경된 파일만 테스트 되도록 수정

--- tests/views/test_post.py
+++ tests/views/test_post.py
@@ -13,14 +13,14 @@ class Describe_PostView:
     class Describe_index:
         @pytest.fixture
         def subject(self, client):
-            url = url_for('PostView:index')
+            url = url_for("PostView:index")
             response = client.get(url)
             return response
 
         class Context_정상_요청:
             def test_200이_반환된다(self, subject):
                 assert subject.status_code == 200
-                assert 'items' in subject.json
+                assert "items" in subject.json
 
         class Context_좋아요_정렬하는_경우:
             @pytest.fixture(autouse=True)
@@ -33,17 +33,17 @@ class Describe_PostView:
 
             @pytest.fixture
             def subject(self, client):
-                url = url_for('PostView:index')
-                params = {'order': 'num_of_likes'}
+                url = url_for("PostView:index")
+                params = {"order": "num_of_likes"}
                 response = client.get(url, query_string=params)
                 return response
 
             def test_200이_반환된다(self, subject):
                 assert subject.status_code == 200
-                assert 'items' in subject.json
-                posts = subject.json['items']
+                assert "items" in subject.json
+                posts = subject.json["items"]
                 for post1, post2 in pairwise(posts):
-                    assert post1['num_of_likes'] >= post2['num_of_likes']
+                    assert post1["num_of_likes"] >= post2["num_of_likes"]
 
         class Context_조회수_정렬하는_경우:
             @pytest.fixture(autouse=True)
@@ -56,32 +56,32 @@ class Describe_PostView:
 
             @pytest.fixture
             def subject(self, client):
-                url = url_for('PostView:index')
-                params = {'order': 'num_of_views'}
+                url = url_for("PostView:index")
+                params = {"order": "num_of_views"}
                 response = client.get(url, query_string=params)
                 return response
 
             def test_200이_반환된다(self, subject):
                 assert subject.status_code == 200
-                assert 'items' in subject.json
-                posts = subject.json['items']
+                assert "items" in subject.json
+                posts = subject.json["items"]
                 for post1, post2 in pairwise(posts):
-                    assert post1['num_of_views'] >= post2['num_of_views']
+                    assert post1["num_of_views"] >= post2["num_of_views"]
 
         class Context_태그_검색을_하는_경우:
             @pytest.fixture(autouse=True)
             def posts(self):
                 return [
-                    PostFactory.create(tags=['tag1', 'tag2']),
-                    PostFactory.create(tags=['tAg1', 'tag3']),
-                    PostFactory.create(tags=['TAG1']),
-                    PostFactory.create(tags=['TAG2']),
-                    PostFactory.create(tags=['tAg3'])
+                    PostFactory.create(tags=["tag1", "tag2"]),
+                    PostFactory.create(tags=["tAg1", "tag3"]),
+                    PostFactory.create(tags=["TAG1"]),
+                    PostFactory.create(tags=["TAG2"]),
+                    PostFactory.create(tags=["tAg3"]),
                 ]
 
             @pytest.fixture
             def tag_keyword(self):
-                return 'TAG1'
+                return "TAG1"
 
             @pytest.fixture
             def expected_posts(self, posts):
@@ -89,37 +89,37 @@ class Describe_PostView:
 
             @pytest.fixture
             def subject(self, client, tag_keyword):
-                url = url_for('PostView:index')
-                response = client.get(url, query_string={'tag': tag_keyword})
+                url = url_for("PostView:index")
+                response = client.get(url, query_string={"tag": tag_keyword})
                 return response
 
             def test_200이_반환된다(self, subject, expected_posts):
                 assert subject.status_code == 200
-                assert 'items' in subject.json
-                posts = subject.json['items']
+                assert "items" in subject.json
+                posts = subject.json["items"]
                 for post, expected_post in zip(posts, expected_posts):
-                    assert post['id'] == str(expected_post['id'])
+                    assert post["id"] == str(expected_post["id"])
 
     class Describe_post:
         @pytest.fixture()
         def form(self):
-            return {'title': 'test title', 'content': 'test content'}
+            return {"title": "test title", "content": "test content"}
 
         @pytest.fixture
         def subject(self, client, headers, form):
-            url = url_for('PostView:post')
+            url = url_for("PostView:post")
             response = client.post(url, headers=headers, json=form)
             return response
 
         class Context_정상_요청:
             def test_게시글이_작성된다_201(self, subject):
                 assert subject.status_code == 201
-                assert subject.json['title'] == 'test title'
+                assert subject.json["title"] == "test title"
 
         class Context_제목이_없는_경우:
             @pytest.fixture()
             def form(self):
-                return {'content': 'test content'}
+                return {"content": "test content"}
 
             def test_400이_반환된다(self, subject):
                 assert subject.status_code == 422
@@ -127,7 +127,7 @@ class Describe_PostView:
         class Context_내용이_없는_경우:
             @pytest.fixture()
             def form(self):
-                return {'title': 'test title'}
+                return {"title": "test title"}
 
             def test_400이_반환된다(self, subject):
                 assert subject.status_code == 422
@@ -143,16 +143,16 @@ class Describe_PostView:
 
         @pytest.fixture
         def subject(self, client, headers, post_id_to_get):
-            url = url_for('PostView:get', post_id=post_id_to_get)
+            url = url_for("PostView:get", post_id=post_id_to_get)
             response = client.get(url, headers=headers)
             return response
 
         class Context_존재하는_게시글_조회:
             def test_200이_반환된다(self, subject, created_post):
                 assert subject.status_code == 200
-                assert subject.json['title'] == created_post.title
-                assert subject.json['content'] == created_post.content
-                assert subject.json['num_of_views'] == created_post.num_of_views + 1
+                assert subject.json["title"] == created_post.title
+                assert subject.json["content"] == created_post.content
+                assert subject.json["num_of_views"] == created_post.num_of_views + 1
 
         class Context_게시글이_없는_경우:
             @pytest.fixture
@@ -173,19 +173,19 @@ class Describe_PostView:
 
         @pytest.fixture()
         def form(self):
-            return {'title': FuzzyText(length=20).fuzz(), 'content': FuzzyText(length=100).fuzz()}
+            return {"title": FuzzyText(length=20).fuzz(), "content": FuzzyText(length=100).fuzz()}
 
         @pytest.fixture
         def subject(self, client, headers, form, created_post, post_id_to_patch):
-            url = url_for('PostView:patch', post_id=post_id_to_patch)
+            url = url_for("PostView:patch", post_id=post_id_to_patch)
             response = client.patch(url, headers=headers, json=form)
             return response
 
         class Context_정상_요청:
             def test_200이_반환된다(self, subject, form):
                 assert subject.status_code == 200
-                assert subject.json['title'] == form['title']
-                assert subject.json['content'] == form['content']
+                assert subject.json["title"] == form["title"]
+                assert subject.json["content"] == form["content"]
 
         class Context_작성한_사용자가_아닌_경우:
             @pytest.fixture
@@ -193,6 +193,7 @@ class Describe_PostView:
                 return PostFactory.create()
 
             def test_403을_반환한다(self, subject):
+                assert subject.status_code == 403
                 assert subject.status_code == 403
 
         class Context_게시글이_없는_경우:
@@ -206,7 +207,7 @@ class Describe_PostView:
         class Context_빈_값을_넣는_경우:
             @pytest.fixture()
             def form(self):
-                return {'title': '', 'content': ''}
+                return {"title": "", "content": ""}
 
             def test_422를_반환한다(self, subject):
                 assert subject.status_code == 422
@@ -218,7 +219,7 @@ class Describe_PostView:
 
         @pytest.fixture
         def subject(self, client, headers, created_post):
-            url = url_for('PostView:delete', id=created_post.id)
+            url = url_for("PostView:delete", id=created_post.id)
             response = client.delete(url, headers=headers)
             return response
 
@@ -241,14 +242,14 @@ class Describe_PostView:
 
         @pytest.fixture
         def subject(self, client, headers, created_post):
-            url = url_for('PostView:likes', id=created_post.id)
+            url = url_for("PostView:likes", id=created_post.id)
             response = client.post(url, headers=headers)
             return response
 
         class Context_정상_요청:
             def test_200이_반환된다(self, subject, created_post):
                 assert subject.status_code == 200
-                assert subject.json['num_of_likes'] == created_post.num_of_likes+1
+                assert subject.json["num_of_likes"] == created_post.num_of_likes + 1
 
         class Context_이미_누른_경우:
             @pytest.fixture
@@ -268,14 +269,14 @@ class Describe_PostView:
 
         @pytest.fixture
         def subject(self, client, headers, created_post):
-            url = url_for('PostView:unlikes', id=created_post.id)
+            url = url_for("PostView:unlikes", id=created_post.id)
             response = client.delete(url, headers=headers)
             return response
 
         class Context_정상_요청:
             def test_200이_반환된다(self, subject, created_post):
                 assert subject.status_code == 200
-                assert subject.json['num_of_likes'] == created_post.num_of_likes-1
+                assert subject.json["num_of_likes"] == created_post.num_of_likes - 1
 
         class Context_이전에_누르지_않은_경우:
             @pytest.fixture
